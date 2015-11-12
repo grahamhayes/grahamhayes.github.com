7a1d46dc3518af1704bfc8f38901a3cf45cfcba6
Graham Hayes <graham.hayes@hpe.com>
2015-11-12 20:11:21 +0000

    Freeze Update

--- graham.hayes.ie/conf.py
+++ graham.hayes.ie/conf.py
@@ -132,7 +132,7 @@ NAVIGATION_LINKS = {
         ('/archive.html', 'Archives', 'icon-folder-open-alt'),
         ('/categories', 'Tags', 'icon-tags'),
         ('/projects', 'Projects', 'icon-briefcase'),
-        ('/talks', 'Talks', 'icon-microphone'),
+        #('/talks', 'Talks', 'icon-microphone'),
         ('/stories/about-me', 'About Me', 'icon-info'),
         ('/rss.xml', 'RSS', 'icon-rss'),
         ('https://twitter.com/grahamhayes', 'My Twitter', 'icon-twitter'),
